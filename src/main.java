2c3b0c63642cde19261d64a5e84fe4e402f1ecd1
DetailsMatterM <moritz.denke@outlook.com>
2021-04-22 15:41:40 +0200

    refactoring

--- src/main.java
+++ src/main.java
@@ -8,11 +8,17 @@ import java.util.ArrayList;
 public class main {
     public static final String DELIMITER = "--------------------------------------------------------------------------------";
 
-    public static void printToFile(ArrayList<String> testNames, String targetDirectory) {
+
+    /**
+     *
+     * @param printData -  String Array, containing data that should be stored in txt
+     * @param targetDirectory - Path to target directory
+     */
+    public static void printToFile(ArrayList<String> printData, String targetDirectory) {
         try {
             FileWriter myWriter = new FileWriter(targetDirectory);
-            for (String name : testNames) {
-                myWriter.write(name + "\n");
+            for (String data : printData) {
+                myWriter.write(data + "\n");
             }
 
             myWriter.close();
@@ -23,30 +29,31 @@ public class main {
         }
     }
 
-    public static void dateParse(String [] input, String [] args) {
+    /**
+     *
+     * @param input - String that contains entire info file
+     * @param outputPath - path to save output
+     */
+    public static void dateParse(String [] input, String outputPath) {
         ArrayList<String> dates = new ArrayList<String>();
         for (int i = 0; i < input.length; i++) {
             if (input[i].equals("date")) {
                 String date = input[i + 3];
+                System.out.println(date);
                 dates.add(date);
             }
         }
-        printToFile(dates, args[1] + "\\" + "date.txt");
+        printToFile(dates, outputPath + "\\" + "date.txt");
     }
+
+
 
     /**
-     * The function provides the framework for analysis of several versions in the basepath
-     * @param basePath - path to project folder with version subfolders
-     * @param firstVersion - first version you want to analyse
-     * @param lastVersion - last version you want to analyse
+     *
+     * @param input - String that contains entire info file
+     * @param outputPath - path to save output
      */
-    public static void multiFileSearch(String basePath, int firstVersion, int lastVersion) {
-        for (int i = 1; i <= lastVersion; i++) {
-            String targetPath = basePath
-        }
-    }
-
-    public static void causeParse(String [] input, String [] args) {
+    public static void causeParse(String [] input, String outputPath) {
         ArrayList<String> testNames = new ArrayList<String>();
         for (int i = 0; i < input.length; i++) {
             if (input[i].equals("cause")) {
@@ -78,22 +85,63 @@ public class main {
         }
         if (testNames.size() > 0) {
             // outputPath has to be given as a file path, like (windows) "D:\\Dropbox\\shared2\\infoParser\\testFiles"
-            printToFile(testNames, args[1] + "\\" + "testNames2.txt");
+            printToFile(testNames, outputPath + "\\" + "testNames2.txt");
         }
     }
 
-    public static void main(String[] args){
+    /**
+     * @param infoFilePath - Contains path to infoFile
+     * @return - returns the info file as a String array
+     */
+    public static String [] infoToString(String infoFilePath) {
         String info = "";
         try {
             // text has to be given as a file path, like (windows) "D:\\Dropbox\\shared2\\infoParser\\testFiles\\testInfo.txt"
-            info = new String(Files.readAllBytes(Paths.get(args[0])));
+            info = new String(Files.readAllBytes(Paths.get(infoFilePath)));
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-
         String [] input = info.split("\\s+");
-        dateParse(input, args);
+        return input;
+    }
+
+    /**
+     * The function provides the framework for analysis of several versions in the basepath
+     * @param basePath - path to project folder with version subfolders
+     * @param firstVersion - first version you want to analyse
+     * @param lastVersion - last version you want to analyse
+     */
+    public static void multiFileSearch(String basePath, int firstVersion, int lastVersion) {
+        for (int i = firstVersion; i <= lastVersion; i++) {
+            String targetPath = basePath + "\\" + "version_" + i;
+            String infoPath = targetPath + "\\testInfo.txt";
+            String [] input = infoToString(infoPath);
+            dateParse(input, targetPath);
+        }
+    }
+
+    /**
+     * args has to contain the arguments below, only 3 arguments are necessary if you
+     * want to parse an info file. They are pathToInfo, outputPath, multifile
+     * while you need 5 arguments if you want to search through multiple files
+     * When you use 5 arguments, outputPath will be ignored
+     * If single file shall be analysed args[0] has to be path to info like ..\Chart\version_2\testInfo.txt
+     * If multiple files will be analysed args [0]has to be the path to the base folder like ..\Chart
+     * @param args - contains: String pathToInfo, String outputpath, String multifile = TRUE/FALSE, int first, int last
+     */
+    public static void main(String[] args){
+
+        if (args[2].equals("TRUE")) {
+            int firstVersion = Integer.parseInt(args[3]);
+            int lastVersion = Integer.parseInt(args[4]);
+            multiFileSearch(args[0], firstVersion,lastVersion);
+        } else {
+            String [] input = infoToString(args[0]);
+            causeParse(input, args[1]);
+        }
+//
+//
 
 
 
